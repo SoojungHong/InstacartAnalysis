90b9e60d92aa31b3b0b551544484be1a683bf3cb
HONG <soojung.hong@atos.net>
2017-08-08 17:43:14 +0200

    data preparation

--- instacart_analysis.py
+++ instacart_analysis.py
@@ -12,6 +12,7 @@ Created on Mon Jul 31 15:56:02 2017
 
 import os
 import pandas as pd
+import numpy as np
 
 
 #-----------------------------------------
@@ -31,6 +32,7 @@ DEPARTMENT_FILE = "departments.csv"
 ORDER_PRIOR_FILE = "order_products__prior.csv"
 ORDER_TRAIN_FILE = "order_products__train.csv"
 ORDERS_FILE = "orders.csv"
+PRODUCT_FILE = "products.csv"
 
 
 """
@@ -48,26 +50,51 @@ aisles_data = readCSV(DATA_PATH, AISLES_FILE)
 dept_data = readCSV(DATA_PATH, DEPARTMENT_FILE)
 #print(dept_data)
 
+product_data = readCSV(DATA_PATH, PRODUCT_FILE)
+print(product_data)
+
 #order_products__prior.csv contains previous order contents for all customers. 'reordered' indicates that the customer has a previous order that contains the product.
 order_prior = readCSV(DATA_PATH, ORDER_PRIOR_FILE) #order_id, product_id, add_to_cart_order, reordered
-#print(order_prior) #32434488
+print(order_prior) #32434488
 
 order_train = readCSV(DATA_PATH, ORDER_TRAIN_FILE) #order_id, product_id, add_to_cart_order, reordered
 #print(order_train) #1384617
 
 #This file tells to which set (prior, train, test) an order belongs. You are predicting reordered items only for the test set orders. 'order_dow' is the day of week.
 orders_data = readCSV(DATA_PATH, ORDERS_FILE) #order_id, user_id, eval_set, order_number, order_dow, order_hour_of_day, days_since_prior_order
-#print(orders_data.info()) #3421083
-#print(orders_data)
+print(orders_data.info()) #3421083
+print(orders_data.head())
 
 #ToDo : divide orders_data using eval_set as 'prior', 'train', 'test' 
 #print(len(orders_data))
 
 orders_test_data = orders_data.loc[orders_data['eval_set'] == 'test'] #75000
-#print(orders_test_data)
+print(orders_test_data)
 
 orders_train_data = orders_data.loc[orders_data['eval_set'] == 'train'] #131209
-#print(orders_train_data)
+print(orders_train_data)
 
 orders_prior_data = orders_data.loc[orders_data['eval_set'] == 'prior'] #3214874
 print(orders_prior_data)
+
+
+#---------------------------------------
+# join data frame for prior and train
+#---------------------------------------
+pd.merge(order_prior, orders_prior_data, on='order_id', how='inner')
+
+pd.merge(order_train, orders_train_data, on='order_id', how='inner')
+
+"""
+raw_data = {
+        'subject_id': ['1', '2', '3', '4', '5', '7', '8', '9', '10', '11'],
+        'test_id': [51, 15, 15, 61, 16, 14, 15, 1, 61, 16]}
+df_n = pd.DataFrame(raw_data, columns = ['subject_id','test_id'])
+df_n
+"""
+#create dataframe of order_test 
+raw_order_test = {'order_id': [np.nan], 'product_id': [np.nan], 'add_to_cart_order': [np.nan], 'reordered': [np.nan] }
+order_test = pd.DataFrame(raw_order_test, columns = ['order_id', 'product_id', 'add_to_cart_order', 'reordered' ])
+print(order_test)
+pd.concat([order_test, orders_test_data], axis=1) #horizontally concatenate
+pd.merge(order_test, orders_test_data, on='order_id', how='outer')

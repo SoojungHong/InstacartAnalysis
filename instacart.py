42b0ad53ee7bfa600e0997a31ea0b10853c15ac1
Soojung <hong.soojung@gmail.com>
2018-05-02 17:56:46 +0200

    initial analysis of raw data

--- instacart.py
+++ instacart.py
@@ -54,6 +54,33 @@ def printAllProductsInfoInOrder(orderID, data, productInfo, product_dept) :
         #print(productInfo[productInfo["product_id"] == ord].product_name)
         print(product_dept[product_dept["product_id"] == ord])
    
+     
+def getProductsInfoInOrder(orderID, data, productInfo, product_dept) :
+    #data = readCSV(path_data, data_order_prior)  
+    #productInfo = readCSV(path_data, data_products)
+    order = data[data['order_id'] == orderID] 
+    if(order.empty) : 
+        print("There is no order in this data set")
+    prodList = order['product_id'].tolist()
+    return prodList
+
+   
+
+def mostFrequentlyPurchasedByUser(userID) : 
+    user_products_id = []
+    ordersPerUser = orders[orders["user_id"] == userID]
+    for ordr in ordersPerUser.order_id : 
+        #print(ordr)
+        currlist = getProductsInfoInOrder(ordr, data, productInfo, product_dept)
+        for p in currlist : 
+            #print(product[product["product_id"] == p].product_name)  
+            user_products_id.append(p)
+    
+    c = Counter(user_products_id)
+    mostfreq_id = c.most_common(1)
+    mostfreq =product.loc[product['product_id'] == mostfreq_id[0][0]] 
+    return mostfreq
+
     
 #-------------
 # read data 
@@ -164,7 +191,6 @@ product_dept[product_dept["product_id"] == 1]
    
 printAllProductsInfoInOrder(6, data, productInfo, product_dept)
 
-
 #---------------------
 # get orders by user 
 #---------------------
@@ -177,24 +203,13 @@ for ord in ordersPerUser.order_id :
     print("")
 
 # most frequently ordered product and department
-ordersPerUser = orders[orders["user_id"] == 1]
+ordersPerUser = orders[orders["user_id"] == 3]
 ordersPerUser.order_id    
  
 for ord_1 in ordersPerUser.order_id : 
     print(ord_1)
     printAllProductsInfoInOrder(ord_1, data, productInfo, product_dept)
-    
-    
-     
-def getProductsInfoInOrder(orderID, data, productInfo, product_dept) :
-    #data = readCSV(path_data, data_order_prior)  
-    #productInfo = readCSV(path_data, data_products)
-    order = data[data['order_id'] == orderID] 
-    if(order.empty) : 
-        print("There is no order in this data set")
-    prodList = order['product_id'].tolist()
-    return prodList
-
+ 
 user_products = []
 for ord_1 in ordersPerUser.order_id : 
     print(ord_1)
@@ -203,9 +218,73 @@ for ord_1 in ordersPerUser.order_id :
         #print(product[product["product_id"] == p].product_name)  
         user_products.append(product[product["product_id"] == p].product_name)
     
+type(user_products)
 user_products
+
+#------------------------------------
+# most frequently purchased product
+#------------------------------------
+user_products_id = []
+for ord_1 in ordersPerUser.order_id : 
+    print(ord_1)
+    list = getProductsInfoInOrder(ord_1, data, productInfo, product_dept)
+    for p in list : 
+        #print(product[product["product_id"] == p].product_name)  
+        user_products_id.append(p)
+        
+user_products_id 
+
+from collections import Counter
+
+c = Counter(user_products_id)
+c.most_common(1)
+product.loc[product['product_id'] == 39190]
+
+mostFrequentlyPurchasedByUser(5)
+
+
+#----------------------------------------------------------
+# For given order, list product list and department list 
+#----------------------------------------------------------
+def getProductAndDept(orderId) : 
+    dop = readCSV(path_data, data_order_prior)
+    aisle = readCSV(path_data, data_aisle)
+    dept = readCSV(path_data, data_dept)    
+    order_given = dop[dop['order_id'] == orderId]
+    products_in_order = order_given['product_id']
+
+    product_info = readCSV(path_data, data_products)
+    product_info
+
+    # join : products and data_aisle and data_dept
+    pro_ais = pd.merge(product_info, aisle, on='aisle_id')
+    pro_dep = pd.merge(product_info, dept, on='department_id')
+
+    pro_location = pd.merge(pro_ais, pro_dep, on='product_id')
+    pro_location
+
+    pro_location['product_name_y']
+
+    product_list = []
+    depart_list = []
+
+    for prod in products_in_order : 
+        print(pro_location[pro_location['product_id'] == prod].product_name_y)
+        pName = pro_location[pro_location['product_id'] == prod].product_name_y
+        product_list.append(pName)
+        print(pro_location[pro_location['product_id'] == prod].department)
+        depName = pro_location[pro_location['product_id'] == prod].department
+        depart_list.append(depName)
+    
+    print(product_list)
+    print(depart_list)   
+    
+
+getProductAndDept(10)
 
 
 # idea 1 : I need NLP package to know the relationship between products in one order
 # idea 2 : I need to know the relationship of ordered product and it's ordered time - is it correlated or not? 
 # idea 3 : customer's preference? 
+# idea 4 : is there any machine learning algorithm for that? 
+# idea 5 : association with each department : there is often related department is order
